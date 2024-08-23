ec5d7e859be729d6e660de6e08f45398ca866c08
eengnr <31403422+eengnr@users.noreply.github.com>
2024-08-23 15:42:03 +0200

    Switch from named pipe to file to avoid blocking

--- pizerow/home/pi/openharmonie/openharmonie.sh
+++ pizerow/home/pi/openharmonie/openharmonie.sh
@@ -4,15 +4,21 @@ mqttpipe="/tmp/mqttpipe";
 
 subscribe() {
 if [[ ! -p "$mqttpipe" ]]; then
-    mkfifo $mqttpipe
+    touch $mqttpipe
 fi
 echo "Subscribe..."
 (mosquitto_sub -h broker.home -t "devices/pi0w/harmonie" -u pi -P pi > $mqttpipe) & # 2>/dev/null) &
-while read line <$mqttpipe
+while true
 do
-    # Execute command in background
-    # Otherwise mosquitto_sub could hang/crash if sending is not finished, but next command is already received
-    (/home/pi/openharmonie/remotes/send_command.sh $line) &
+    read line < $mqttpipe
+    if [[ ! -z $line ]]
+    then
+        # Execute command in background
+        # Otherwise mosquitto_sub could hang/crash if sending is not finished, but next command is already received
+        (/home/pi/openharmonie/remotes/send_command.sh $line) &
+        : > $mqttpipe
+    fi
+    sleep .1
 done
 }
 
@@ -24,7 +30,7 @@ do
         echo "mosquitto_sub not running anymore..."
         shutdown
     fi
-    if [[ ! -p "$mqttpipe" ]]; then
+    if [[ ! -e "$mqttpipe" ]]; then
         echo "pipe not available anymore..."
         shutdown
     fi
@@ -35,7 +41,7 @@ shutdown() {
   echo "Shutdown..."
   rm $mqttpipe
   if pgrep "mosquitto_sub" > /dev/null; then
-      kill $(pidof mosquitto_sub) 2>/dev/null;
+      pkill -9 mosquitto_sub 2>/dev/null;
   fi
   exit
 }
